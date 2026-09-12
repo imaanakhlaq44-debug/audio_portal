e02d9b0442272684d74ba6ff1b7d9333390ef015
Jawad Almas <jawadahmadalmas@gmail.com>
2026-09-12 19:26:45 +0000

    Android: register audio_service foreground service, media-button receiver, permissions; MainActivity extends AudioServiceActivity

--- android/app/src/main/kotlin/com/imaanakhlaq/stories/MainActivity.kt
+++ android/app/src/main/kotlin/com/imaanakhlaq/stories/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.imaanakhlaq.stories
 
-import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity
 
-class MainActivity : FlutterActivity()
+/// Extends AudioServiceActivity (instead of FlutterActivity) so the
+/// audio_service plugin can attach its media session to this activity and
+/// tapping the playback notification brings the app back to the foreground.
+class MainActivity : AudioServiceActivity()
